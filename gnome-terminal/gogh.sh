aa5609c8fb25ffc0a17ea77ff89509e7894c263b
pacbru1260 <75707332+pacbru1260@users.noreply.github.com>
2022-06-24 17:43:38 -0300

    Update gogh.sh

--- gnome-terminal/gogh.sh
+++ gnome-terminal/gogh.sh
@@ -1,1 +1,9 @@
-bash -c  "$(wget -qO- https://git.io/vQgMr)" 
+echo "This script assumes you have Git installed in your system!" 
+git clone https://github.com/modus-theme/gnome-terminal
+cd gnome-terminal
+chmod +x ./install-vivendi.sh
+chmod +x ./install-operandi.sh
+./install-vivendi.sh
+./install-operandi.sh
+echo "This script also assumes that you are still inside the gnome-terminal directory, created by this script, so be careful! (If you lose files and/or directories, it's not my fault"
+exit 0
