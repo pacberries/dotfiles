2b7f709bbddd0340a90ccd2b2edef1a8f7f579fc
pacbru1260 <75707332+pacbru1260@users.noreply.github.com>
2022-06-24 17:45:49 -0300

    Update gogh.sh

--- gnome-terminal/gogh.sh
+++ gnome-terminal/gogh.sh
@@ -5,5 +5,4 @@ chmod +x ./install-vivendi.sh
 chmod +x ./install-operandi.sh
 ./install-vivendi.sh
 ./install-operandi.sh
-echo "This script also assumes that you are still inside the gnome-terminal directory, created by this script, so be careful! (If you lose files and/or directories, it's not my fault"
 exit 0
