214aa409372ac7cbd92c1217ea7d148819b1ee07
pacbru1260 <75707332+pacbru1260@users.noreply.github.com>
2022-06-26 19:01:10 -0300

    Update and rename modus.sh to gogh.sh

--- gnome-terminal/modus.sh
+++
@@ -1,8 +0,0 @@
-echo "This script assumes you have Git installed in your system!" 
-git clone https://github.com/modus-theme/gnome-terminal
-cd gnome-terminal
-chmod +x ./install-vivendi.sh
-chmod +x ./install-operandi.sh
-./install-vivendi.sh
-./install-operandi.sh
-exit 0
