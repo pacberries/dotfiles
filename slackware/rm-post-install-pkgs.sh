34817ae9357cf864b371d8aab3c93a74910a52f9
pacbru1260 <75707332+pacbru1260@users.noreply.github.com>
2022-07-10 14:22:59 -0300

    Create rm-post-install-pkgs.sh

---
+++ slackware/rm-post-install-pkgs.sh
@@ -0,0 +1,2 @@
+echo "This will remove the following packages: kde xaos MPlayer easytag xine seamonkey zenmap pan joe gpa pia xnow gftp"
+slackpkg remove kde xaos MPlayer easytag xine seamonkey zenmap pan joe gpa pia xnow gftp
